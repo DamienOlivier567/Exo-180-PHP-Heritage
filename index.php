37e7e738f20310541ac3e6d397262dd4549efc12
DamienOlivier567 <73164252+DamienOlivier567@users.noreply.github.com>
2021-02-09 09:21:16 +0100

    Exo-180-PHP

--- index.php
+++ index.php
@@ -28,9 +28,48 @@
  *
  *
  *
- * 4. Créez deux instances de Maison et invoquez toutes les méthodes définies, sauf les setters ( dans index.php ).
+ * 4. Créez deux instances de Maison et invoquez toutes les méthodes définies, sauf les setters (dans index.php).
  *
  *
  *
- * 5. Créez une instance de Appartement et invoquez toutes les méthodes définies, y compris les setters ( dans index.php ).
+ * 5. Créez une instance de Appartement et invoquez toutes les méthodes définies, y compris les setters (dans index.php).
  */
+
+require "./classes/Habitation.php";
+require "./classes/Maisons.php";
+require "./classes/Appartement.php";
+
+$myHouse1 = new Maison("France", "Anor", 59186, 4, 10, true, 0, true);
+echo $myHouse1->getPays()."<br>";
+echo $myHouse1->getVille()."<br>";
+echo $myHouse1->getCodePostale()."<br>";
+echo $myHouse1->getChambres()."<br>";
+echo $myHouse1->getPieces()."<br>";
+echo $myHouse1->isJardin()."<br>";
+echo $myHouse1->getEtage()."<br>";
+echo $myHouse1->isGarage()."<br><br>";
+
+$myHouse2 = new Maison("France", "Fourmies", 59610, 2, 4, false, 2, false);
+echo $myHouse2->getPays()."<br>";
+echo $myHouse2->getVille()."<br>";
+echo $myHouse2->getCodePostale()."<br>";
+echo $myHouse2->getChambres()."<br>";
+echo $myHouse2->getPieces()."<br>";
+echo $myHouse2->isJardin()."<br>";
+echo $myHouse2->getEtage()."<br>";
+echo $myHouse2->isGarage()."<br><br>";
+
+$myApps1 = new Appartement("Bordeciel", "Fort-Dragon", 0006, 7, 5, false);
+echo $myApps1->getPays()."<br>";
+echo $myApps1->getVille()."<br>";
+echo $myApps1->getCodePostale()."<br>";
+echo $myApps1->getChambres()."<br>";
+echo $myApps1->getPieces()."<br>";
+echo $myApps1->isGarage()."<br><br>";
+
+$myApps1->setPays("Velen");
+$myApps1->setVille("Novigrad");
+$myApps1->setCodePostale(547);
+$myApps1->setChambres(47);
+$myApps1->setPieces(54);
+$myApps1->setGarage(false);
